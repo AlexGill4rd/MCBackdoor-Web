6c6b7282c5034bfce6ec3ae3be8e14392ee68f1e
Alex <57497005+KingDevCode@users.noreply.github.com>
2022-06-28 16:40:18 +0200

    Begin restyling homepage

--- Client/src/pages/homepage/HomePage.tsx
+++ Client/src/pages/homepage/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './styling/HomePageStyling.scss';
@@ -7,12 +6,19 @@ function HomePage(){
 
     return (
         <div className="homepage-container">
-            <div className='homepage-buttons'>
-                <Link to='/controller/homepage'>
-                    <div className='homepage-buttons-home'>
-                        <div className='homepage-buttons-home-title'>Home</div>                  
-                    </div>
-                </Link>
+            <div className='homepage-panel'>
+                <div className='homepage-left'>
+                    <Link to='/controller/homepage'>
+                        <div className='homepage-left-button'>
+                            <div className='homepage-buttons-button-title'>Home</div>                  
+                        </div>
+                    </Link>
+                </div>
+                <div className='homepage-right'>
+                    <h1>Virus V5</h1>
+                    <h2>Waarvoor dit panel?</h2>
+                    <p>Het paneel is psecifiek ontworpen om op basis van een minecraft plugin in staat te zijn een server via deze webpagina compleet over te namen. De persoon met toegang tot de webpagina is in staat de server te beheren naar zijn eisen.</p>
+                </div>
             </div>
         </div>
     );
