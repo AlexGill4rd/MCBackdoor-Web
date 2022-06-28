622b95a21d09554c4ca571d9702fed85545b5bd5
Alex <57497005+KingDevCode@users.noreply.github.com>
2022-06-28 23:12:11 +0200

    Fixed homepage tabs

--- Client/src/pages/homepage/HomePage.tsx
+++ Client/src/pages/homepage/HomePage.tsx
@@ -10,7 +10,7 @@ function HomePage(){
         <div className="homepage-container">
             <div className='homepage-panel'>
                 <div className='homepage-panel-info'>
-                    <div className='homepage-tab' style={{borderRadius: "20px 0 0 0"}}>
+                    <div className='homepage-tab2' style={{borderRadius: "20px 0 0 0"}}>
                         <img src='/icons/homepage/home.png' alt='home icon' />
                         <h4>Waarom heb ik dit gemaakt?</h4>
                         <p>Een project gemaakt om mijn kennis uit te breiden. Uit het maken van projecten leer je het meest.</p>
@@ -20,7 +20,7 @@ function HomePage(){
                         <h4>Hoe werkt het?</h4>
                         <p>Bij het injecteren van de code in een minecraft plugin zal jij als eigenaar in staat zijn toegang te krijgen tot de complete server.</p>
                     </div>
-                    <div className='homepage-tab' style={{borderRadius: "0 0 0 20px"}}>
+                    <div className='homepage-tab2' style={{borderRadius: "0 0 0 20px"}}>
                         <img src='/icons/homepage/folder.png' alt='home icon' />
                         <h4>Wat kan je doen?</h4>
                         <p>Met het paneel kan je de voledige contole krijgen over een bepaalde server. Zo kan je de file's aanpassen en meer.</p>
