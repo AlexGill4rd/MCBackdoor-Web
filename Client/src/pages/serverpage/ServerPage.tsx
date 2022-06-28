f6b3bed54f712fa05805fe109b34ac70eb0c4b9d
Alex <57497005+KingDevCode@users.noreply.github.com>
2022-06-28 22:47:18 +0200

    ServerlistPage restyles

--- Client/src/pages/serverpage/ServerPage.tsx
+++ Client/src/pages/serverpage/ServerPage.tsx
@@ -1,4 +1,6 @@
+import { Tooltip } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../globaltsx/Header';
 import ServerList from './components/ServerList';
 
@@ -7,12 +9,15 @@ import './styling/ServerPageStyling.scss';
 function ServerPage(){
 
     return (
-        <>
-            <Header title='Servers' path='/controller/homepage' />
-            <div className="serverpage-container">
-                <ServerList />
-            </div>
-        </>
+
+        <div className="serverpage-container">
+            <Tooltip title="Ga terug naar vorige pagina!">
+                <Link to='/controller/homepage'>
+                    <h1>Server Lijst</h1>
+                </Link>
+            </Tooltip>
+            <ServerList />
+        </div>
     );
 }
 export default ServerPage;
